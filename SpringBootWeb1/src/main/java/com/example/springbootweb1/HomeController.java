47fa8c0acecc67edb2d66cc3a8eabc57b0a81cf8
Harshit Bole <=>
2024-05-09 15:48:20 +0530

    SpringWeb Initialized

---
+++ SpringBootWeb1/src/main/java/com/example/springbootweb1/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.springbootweb1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        System.out.println("Home method called...");
+        return "index.jsp";
+    }
+}
---
+++ SpringBootWeb1/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<%@page language="java" %>
+<html>
+    <body>
+        <h1>Hello World!!</h1>
+    </body>
+</html>
