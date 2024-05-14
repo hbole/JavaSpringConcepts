47f540a868063d33df10a624baae5d28f9418433
Harshit Bole <harshitdon3@gmail.com>
2024-05-14 14:34:31 +0530

    Final Commit To Spring Data Rest

---
+++ SpringOAuthEx/src/main/java/com/example/springoauthex/SpringOAuthExApplication.java
@@ -0,0 +1,13 @@
+package com.example.springoauthex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOAuthExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOAuthExApplication.class, args);
+    }
+
+}
